c9463bea3041e23bb4bb36b5b8cdce5b4c2c6034
Skyler Lutz <splutz@solstice-consulting.com>
2017-01-10 15:08:41 -0600

    Collectibles

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MovementController : NetworkBehaviour {
+
+	// Use this for initialization
+	void OnStartLocalPlayer() {
+		GetComponent<Material>().color = Color.blue;
+	}
+
+	void Update() {
+
+		if (!isLocalPlayer) {
+			return;
+		}
+
+		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 5.0f;
+		var z = Input.GetAxis ("Vertical") * Time.deltaTime * 5.0f;
+
+		transform.Translate (new Vector3(x, 0.0f, z));
+
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,21 +5,17 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour {
 
-	// Use this for initialization
-	void OnStartLocalPlayer() {
-		GetComponent<Material>().color = Color.blue;
-	}
+	// prefab to instantiate to spawn a collectible
+	public GameObject collectible; 
 
-	void Update() {
+	// Update is called once per frame
+	void Update () {
 
-		if (!isLocalPlayer) {
-			return;
+		if (Input.GetMouseButtonDown (0)) {
+
+			GameObject obj = (GameObject)Instantiate (collectible, transform.position, transform.rotation);
+
+			NetworkServer.Spawn (obj);
 		}
-
-		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 5.0f;
-		var z = Input.GetAxis ("Vertical") * Time.deltaTime * 5.0f;
-
-		transform.Translate (new Vector3(x, 0.0f, z));
-
 	}
 }
