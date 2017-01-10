a5be4204882a622f1c2e3500440d91770c6b1550
Skyler Lutz <splutz@solstice-consulting.com>
2017-01-10 15:55:43 -0600

    WIP

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,14 +8,27 @@ public class PlayerController : NetworkBehaviour {
 	// prefab to instantiate to spawn a collectible
 	public GameObject collectible; 
 
+	void Start() {
+//		if (isServer) {
+//			ClientScene.RegisterPrefab (collectible);
+//		}
+	}
+
 	// Update is called once per frame
 	void Update () {
 
 		if (Input.GetMouseButtonDown (0)) {
+//			CmdSpawn ();
 
-			GameObject obj = (GameObject)Instantiate (collectible, transform.position, transform.rotation);
-
-			NetworkServer.Spawn (obj);
+			Network.Instantiate (collectible, transform.position, transform.rotation);
 		}
 	}
+
+	[Command]
+	public void CmdSpawn() {
+
+		GameObject obj = (GameObject)Instantiate (collectible, transform.position, transform.rotation);
+		NetworkServer.Spawn (obj);
+	}
+
 }
